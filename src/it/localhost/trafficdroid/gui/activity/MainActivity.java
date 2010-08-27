8d568babc28780833e5addb5f386b91bafcab427
Alessandro Scarozza <xan.scale@gmail.com>
2010-08-27 21:17:55 +0000

    don't panic

--- src/it/localhost/trafficdroid/gui/activity/MainActivity.java
+++ src/it/localhost/trafficdroid/gui/activity/MainActivity.java
@@ -2,11 +2,10 @@ package it.localhost.trafficdroid.gui.activity;
 
 import it.localhost.trafficdroid.R;
 import it.localhost.trafficdroid.common.Const;
-import it.localhost.trafficdroid.core.Parser;
 import it.localhost.trafficdroid.dao.StreetDAO;
 import it.localhost.trafficdroid.dto.StreetDTO;
-import it.localhost.trafficdroid.exception.CoreException;
 import it.localhost.trafficdroid.gui.adapter.ZoneListAdapter;
+import it.localhost.trafficdroid.gui.task.DLCTask;
 
 import java.util.List;
 
@@ -14,7 +13,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -105,35 +103,5 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	private class DLCTask extends AsyncTask<List<StreetDTO>, Void, List<StreetDTO>> {
-		private String error;
-
-		@Override
-		protected void onPreExecute() {
-			error = null;
-		}
-
-		@Override
-		protected List<StreetDTO> doInBackground(List<StreetDTO>... params) {
-			error = null;
-			try {
-				for (StreetDTO elem : params[0])
-					elem = Parser.parse(elem, url);
-				return params[0];
-			} catch (CoreException e) {
-				error = e.getKey() + ": " + e.getMessage();
-				return null;
-			}
-		}
-
-		@Override
-		protected void onPostExecute(List<StreetDTO> tratte) {
-			if (error != null)
-				new AlertDialog.Builder(MainActivity.this).setTitle(getResources().getText(R.string.error)).setMessage(error).setPositiveButton(getResources().getText(R.string.ok), null).show();
-			else {
-				allEnabledStreets = tratte;
-				setView();
-			}
-		}
-	}
+	
 }
