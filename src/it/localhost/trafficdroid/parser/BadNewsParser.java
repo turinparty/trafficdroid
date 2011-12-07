4502a9f28d68b69564c9b9c713d645e5631ac8e0
Alessandro Scarozza <xan.scale@gmail.com>
2011-12-07 17:14:54 +0000

    0.10.1 skip on badnews parser timeout

--- src/it/localhost/trafficdroid/parser/BadNewsParser.java
+++ src/it/localhost/trafficdroid/parser/BadNewsParser.java
@@ -5,29 +5,30 @@ import it.localhost.trafficdroid.dao.EventDAO;
 import it.localhost.trafficdroid.dto.BadNewsDTO;
 import it.localhost.trafficdroid.dto.MainDTO;
 import it.localhost.trafficdroid.dto.StreetDTO;
-import it.localhost.trafficdroid.exception.BadConfException;
-import it.localhost.trafficdroid.exception.ConnectionException;
-import it.localhost.trafficdroid.exception.GenericException;
 
 import java.util.StringTokenizer;
 
 import org.w3c.dom.NodeList;
 
 public class BadNewsParser {
-	public static void parse(MainDTO dto, String url) throws GenericException, BadConfException, ConnectionException {
-		NodeList items = EventDAO.getData(url).getDocumentElement().getElementsByTagName(Const.item);
-		for (int i = 0; i < items.getLength(); i++) {
-			NodeList item = items.item(i).getChildNodes();
-			String street = new StringTokenizer(item.item(1).getTextContent(), Const.badNewsStreetDelim).nextToken();
-			StringTokenizer descST = new StringTokenizer(item.item(3).getTextContent(), Const.badNewsDelim);
-			if (street.charAt(0) == Const.charAutostrade)
-				try {
-					for (StreetDTO streetDTO : dto.getStreets())
-						if (Integer.parseInt(street.substring(1, street.length())) == streetDTO.getId())
-							streetDTO.addBadNews(new BadNewsDTO(descST.nextToken(), descST.nextToken(), Const.sdfBnParse.parse(item.item(7).getTextContent())));
-				} catch (Exception e) {
-					// Do nothing
-				}
+	public static void parse(MainDTO dto, String url) {
+		try {
+			NodeList items = EventDAO.getData(url).getDocumentElement().getElementsByTagName(Const.item);
+			for (int i = 0; i < items.getLength(); i++) {
+				NodeList item = items.item(i).getChildNodes();
+				String street = new StringTokenizer(item.item(1).getTextContent(), Const.badNewsStreetDelim).nextToken();
+				StringTokenizer descST = new StringTokenizer(item.item(3).getTextContent(), Const.badNewsDelim);
+				if (street.charAt(0) == Const.charAutostrade)
+					try {
+						for (StreetDTO streetDTO : dto.getStreets())
+							if (Integer.parseInt(street.substring(1, street.length())) == streetDTO.getId())
+								streetDTO.addBadNews(new BadNewsDTO(descST.nextToken(), descST.nextToken(), Const.sdfBnParse.parse(item.item(7).getTextContent())));
+					} catch (Exception e) {
+						// Do nothing
+					}
+			}
+		} catch (Exception e1) {
+			// Do nothing
 		}
 	}
 }
