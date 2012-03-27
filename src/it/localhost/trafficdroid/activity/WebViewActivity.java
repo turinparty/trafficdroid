6fa69175b1ba5475633b9e6b1aac701d112887f4
Alessandro Scarozza <xan.scale@gmail.com>
2012-03-27 23:01:52 +0000

    codeclean

--- src/it/localhost/trafficdroid/activity/WebViewActivity.java
+++ src/it/localhost/trafficdroid/activity/WebViewActivity.java
@@ -12,7 +12,6 @@ public class WebViewActivity extends AbstractActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TdAnalytics.startNewSession(Const.anlyticsId);
 		setContentView(R.layout.webview);
 		webView = (WebView) findViewById(R.id.webview);
 		webView.getSettings().setJavaScriptEnabled(true);
@@ -23,23 +22,5 @@ public class WebViewActivity extends AbstractActivity {
 			webView.loadUrl(url);
 		else if (data != null)
 			webView.loadData(data, Const.html, Const.utf8);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		TdAnalytics.trackPageView(this.getClass().getName());
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		TdAnalytics.dispatch();
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-		TdAnalytics.stopSession();
 	}
 }
