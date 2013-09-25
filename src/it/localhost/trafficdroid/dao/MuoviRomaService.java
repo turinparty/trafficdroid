ec9e72c3b3475ae4f49caee24847504bc4dc0884
xan.scale <xan.scale@gmail.com>
2013-09-25 23:14:26 +0200

    better implementation

--- src/it/localhost/trafficdroid/dao/MuoviRomaService.java
+++ src/it/localhost/trafficdroid/dao/MuoviRomaService.java
@@ -3,34 +3,48 @@ package it.localhost.trafficdroid.dao;
 import it.localhost.trafficdroid.dto.BaseDTO;
 import it.localhost.trafficdroid.dto.MuoviRomaDTO;
 
+import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
 import android.os.AsyncTask;
 
 public class MuoviRomaService extends AsyncTask<String, Void, BaseDTO> {
-	private static final String URL = "http://muovi.roma.it/ztl/";
-	private static final String VALUE = "red_bullet.gif";
-	private static final String NAME = "<h3>";
-	private static final String FILE = "\\A";
-	private static final String LINE = "\n";
+	private static final String P = "p";
+	private static final String H3 = "h3";
+	private static final String SPAN_CLASS_I = "<span class=\"i\">";
+	private static final String START = "<h2 class=\"vskip\">Orari di oggi</h2>";
+	private static final String END = "<p class=\"vskip\">";
+	private static final String A_CLOSE = "</a>";
+	private static final String A_OPEN = "<a>";
+	private static final String SPAN_CLOSE = "</span>";
+	private static final String SPAN_OPEN = "<span>";
 	private static final String BLANK = "";
+	private static final String URL = "http://muovi.roma.it/ztl/";
+	private static final String FILE = "\\A";
 
 	@Override
 	protected BaseDTO doInBackground(String... args) {
 		try {
 			ArrayList<String[]> out = new ArrayList<String[]>();
-			for (String line : new Scanner(new URL(URL).openStream()).useDelimiter(FILE).next().split(LINE)) {
-				if (line.contains(NAME) || line.contains(VALUE)) {
-					if (line.contains(NAME))
-						out.add(new String[] { line.trim().substring(4, line.length() - 7), BLANK });
-					else if (line.contains(VALUE))
-						out.get(out.size() - 1)[1] = line.trim().substring(105, line.length() - 10);
-				}
+			String data = new Scanner(new URL(URL).openStream()).useDelimiter(FILE).next().replace(SPAN_CLASS_I, BLANK).replace(SPAN_OPEN, BLANK).replace(SPAN_CLOSE, BLANK);
+			int start = data.indexOf(START);
+			int end = data.indexOf(END, start);
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(A_OPEN + data.substring(start, end) + A_CLOSE)));
+			NodeList nl = doc.getChildNodes().item(0).getChildNodes();
+			for (int i = 0; i < nl.getLength(); i++) {
+				if (nl.item(i).getNodeName().equals(H3))
+					out.add(new String[] { nl.item(i).getTextContent(), BLANK });
+				else if (nl.item(i).getNodeName().equals(P))
+					out.get(out.size() - 1)[1] = nl.item(i).getTextContent().trim();
 			}
-			for (String[] strings : out)
-				System.err.println(strings[0] + ":" + strings[1]);
 			return new MuoviRomaDTO(true, out);
 		} catch (Exception e) {
 			e.printStackTrace();
